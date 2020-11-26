84df9ba738a1d0995ac515f4aafa521558aad0f0
Arielle ross <arielle@arielleross.com>
2020-11-26 21:34:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@ require "yaml"
 require 'pry'
 def load_library(file)
   emoticons = YAML.load_file(file)
-library = emoticons.each {|key, value| puts "#{key} is #{value}" }
+emoticons[0]
 binding.pry
 end
 
