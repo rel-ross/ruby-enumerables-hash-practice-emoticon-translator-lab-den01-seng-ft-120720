1df1ed448b0c6a662d7bd32aaeb1442ee7d26ed4
Arielle ross <arielle@arielleross.com>
2020-11-26 22:03:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,7 +12,7 @@ def load_library(file)
 end
 
 def get_japanese_emoticon(file, emoticon_request)
-  emoticons = YAML.load_file(file)
+YAML.load_file(file)
  japanese_translation = emoticons[emoticon_request][1]
  
   # code goes here
